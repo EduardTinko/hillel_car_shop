4d9e0475ef623053edef256d9014a3f477eb964e
TinkoEd <228rubiks228@gmail.com>
2023-12-13 21:18:43 +0200

    Логін через google sso1

--- car/forms.py
+++ car/forms.py
@@ -1,21 +1,33 @@
+from allauth.account.forms import LoginForm, SignupForm
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.urls import reverse, NoReverseMatch
+from django.utils.safestring import mark_safe
+from django.utils.translation import gettext as _
 
 from .models import Car, CarType, Dealership, Order, OrderQuantity, Licence
 
 
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField()
-
+class MyCustomSignupForm(SignupForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.fields["password1"].help_text = ""
 
-    class Meta:
-        model = User
-        fields = ["username", "email", "password1", "password2"]
+
+class MyCustomLoginForm(LoginForm):
+    remember = forms.BooleanField(label=_("Запам'ятати мене"), required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            reset_url = reverse("account_reset_password")
+        except NoReverseMatch:
+            pass
+        else:
+            forgot_txt = _("<p> Забули пароль?")
+            self.fields["password"].help_text = mark_safe(
+                f'<a href="{reset_url}">{forgot_txt}</a>'
+            )
 
 
 class CarTypeForm(forms.Form):
