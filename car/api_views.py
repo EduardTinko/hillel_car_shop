55f612737d41630fd5970d691afd431815e5f6b7
TinkoEd <228rubiks228@gmail.com>
2024-01-30 12:39:07 +0200

    file

--- car/api_views.py
+++ car/api_views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.reverse import reverse
+import rest_framework.reverse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets, status, mixins
@@ -144,7 +144,7 @@ class ConfirmOrderAPIView(APIView):
     def post(self, request, order_id, *args, **kwargs):
         order = self._get_order(order_id=order_id)
         self.check_object_permissions(request, order)
-        create_invoice(order, reverse("webhook-mono", request=request))
+        create_invoice(order, rest_framework.reverse.reverse("webhook-mono", request=request))
         return Response({"invoice_url": order.invoice_url})
 
     def delete(self, request, order_id, *args, **kwargs):
